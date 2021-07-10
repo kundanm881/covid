c9a48e805e53dc42f73b242f52286547fc1d77fb
BuildTools <unconfigured@null.spigotmc.org>
2021-07-10 17:39:49 +0530

    theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:theme_provider/theme_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Covid App",
-      home: Scaffold(
-        body: Center(child: Text("hello world"),),
+    return ThemeProvider(
+      themes: [
+        AppTheme.light(), // This is standard light theme (id is default_light_theme)
+        AppTheme.dark(), // This is standard dark theme (id is default_dark_theme)
+        AppTheme(
+          id: "custom_theme", // Id(or name) of the theme(Has to be unique)
+          description: "My Custom Theme", // Description of theme
+          data: ThemeData(  // Real theme data
+            primaryColor: Colors.black,
+            accentColor: Colors.red,
+          ),
+        ),
+      ],
+      child: ThemeConsumer(
+        child: Builder(
+          builder: (themeContext) => MaterialApp(
+            theme: ThemeProvider.themeOf(themeContext).data,
+            title: 'Material App',
+            home: HomePage(),
+          ),
+        ),
       ),
     );
   }
 }
+
+class HomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("kundan"),),
+      body: Center(child: RaisedButton(onPressed: (){
+      ThemeProvider.controllerOf(context).setTheme("custom_theme");
+      // print(ThemeProvider.controllerOf(context));
+    },child: Text("theme"),),),);
+  }
+}
+
