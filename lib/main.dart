289d5ca557cfc4f382579f2711c1dc6f91b22f01
BuildTools <unconfigured@null.spigotmc.org>
2021-07-10 21:08:37 +0530

    new theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,30 @@
-import 'package:flutter/material.dart';
-import 'package:theme_provider/theme_provider.dart';
 
-void main() => runApp(MyApp());
+import 'package:covid/services/mytheme.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(ChangeNotifierProvider<ThemeNotifier>(
+  create: (_) => ThemeNotifier(ThemeMode.system),
+  child: MyApp(),
+),);
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ThemeProvider(
-      themes: [
-        AppTheme.light(), // This is standard light theme (id is default_light_theme)
-        AppTheme.dark(), // This is standard dark theme (id is default_dark_theme)
-        AppTheme(
-          id: "custom_theme", // Id(or name) of the theme(Has to be unique)
-          description: "My Custom Theme", // Description of theme
-          data: ThemeData(  // Real theme data
-            primaryColor: Colors.black,
-            accentColor: Colors.red,
+    final _themeNotifier = Provider.of<ThemeNotifier>(context);
+    return Consumer<ThemeNotifier>(
+      builder: (context,themeNotifier,child){
+        return MaterialApp(
+          theme: lightTheme,
+          darkTheme: darkTheme,
+          themeMode: themeNotifier.getTheme(),
+          home: Scaffold(
+            appBar: AppBar(
+              title: Text("mandal ${themeNotifier.getTheme()}"),
+            ),
           ),
-        ),
-      ],
-      child: ThemeConsumer(
-        child: Builder(
-          builder: (themeContext) => MaterialApp(
-            theme: ThemeProvider.themeOf(themeContext).data,
-            title: 'Material App',
-            home: HomePage(),
-          ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
-
-class HomePage extends StatefulWidget {
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("kundan"),),
-      body: Center(child: RaisedButton(onPressed: (){
-      ThemeProvider.controllerOf(context).setTheme("custom_theme");
-      // print(ThemeProvider.controllerOf(context));
-    },child: Text("theme"),),),);
-  }
-}
-
